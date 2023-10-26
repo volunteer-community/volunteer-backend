2e67508c8c864b83cf098768046052815f4a39a8
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-26 23:08:19 +0900

    :sparkles: 회원가입 페이지 생성

---
+++ src/main/java/com/maple/volunteer/dto/user/SignupDto.java
@@ -0,0 +1,20 @@
+package com.maple.volunteer.dto.user;
+
+import com.maple.volunteer.type.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignupDto {
+    private String email;
+    private String name;
+    private String picture;
+    private String provider;
+    private String nickname;
+    private String phoneNumber;
+    private Role role;
+}
