06c2c9f9fcdb5e808edc2de2171c58227ef5fde5
Llimy1 <alsgur990104@gmail.com>
2023-10-29 17:18:41 +0900

    :recycle: 사용하지 않는 파일 삭제

--- src/main/java/com/maple/volunteer/exception/FailNewCreateFileException.java
+++
@@ -1,9 +0,0 @@
-package com.maple.volunteer.exception;
-
-import com.maple.volunteer.type.ErrorCode;
-
-public class FailNewCreateFileException extends RuntimeException {
-    public FailNewCreateFileException(ErrorCode errorCode) {
-        super(errorCode.getDescription());
-    }
-}
