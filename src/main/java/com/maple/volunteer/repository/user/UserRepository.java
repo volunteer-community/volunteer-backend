0ab9009295b8a05eded58211808e7bed3024cc2a
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-31 01:40:14 +0900

    :sparkles: findActiveUserByEmail 추가

--- src/main/java/com/maple/volunteer/repository/user/UserRepository.java
+++ src/main/java/com/maple/volunteer/repository/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.maple.volunteer.repository.user;
 
 import com.maple.volunteer.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String userEmail);
     User findByPhoneNumber(String phoneNumber);
     User findByNickname(String nickname);
+
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false AND u.email = :email")
+    Optional<User> findActiveUserByEmail(@Param("email") String email);
 }
