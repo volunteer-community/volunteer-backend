4cf79aed7b2c60a4a51cbda191a39d27fdc64b36
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-30 18:30:07 +0900

    :recycle: 주석 처리

--- src/main/java/com/maple/volunteer/repository/comment/CommentRepository.java
+++ src/main/java/com/maple/volunteer/repository/comment/CommentRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END "
-            + "FROM Comment cm "
-            + "LEFT JOIN p.communityUser cu "
-            + "LEFT JOIN cu.community c "
-            + "WHERE c.id = :communityId")
-    Optional<Boolean> existsByPosterId(Long posterId);
+//    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END "
+//            + "FROM Comment cm "
+//            + "LEFT JOIN p.communityUser cu "
+//            + "LEFT JOIN cu.community c "
+//            + "WHERE c.id = :communityId")
+//    Optional<Boolean> existsByPosterId(Long posterId);
 }
