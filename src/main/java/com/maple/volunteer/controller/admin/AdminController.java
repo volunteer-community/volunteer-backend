b993645a9339189620cceb03a4ecf11166c98c08
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-30 18:32:31 +0900

    :sparkles: admin, mypage controller, service 추가

---
+++ src/main/java/com/maple/volunteer/controller/admin/AdminController.java
@@ -0,0 +1,9 @@
+package com.maple.volunteer.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+}
---
+++ src/main/java/com/maple/volunteer/service/mypage/MyPageService.java
@@ -0,0 +1,9 @@
+package com.maple.volunteer.service.mypage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageService {
+}
