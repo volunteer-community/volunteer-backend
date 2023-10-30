3271d1e6abeab7595f82b340c0f0f45d8c245c74
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-31 01:41:22 +0900

    :sparkles: addInfo 추가 및 JWT 관련 코드 개선

--- src/main/java/com/maple/volunteer/service/user/UserService.java
+++ src/main/java/com/maple/volunteer/service/user/UserService.java
@@ -17,6 +17,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -28,16 +31,17 @@ public class UserService {
     private final JwtUtil jwtUtil;
 
 
-
-    public CommonResponseDto<Object> login(String email, String role) {
+    // 로그인
+    public CommonResponseDto<Object> login(String email) {
+        // email로 User(false) get
+        User user = userRepository.findActiveUserByEmail(email)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+        Long userId = user.getId();
 
         // accessToken, refreshToken 발행
-        GeneratedToken token = jwtUtil.generateToken(email, role);
+        GeneratedToken token = jwtUtil.generateToken(userId);
 
         // 기존 refreshToken 변경
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
-
         Login login = user.getLogin();
         loginRepository.updateRefreshTokenById(login.getId(), token.getRefreshToken());
 
@@ -53,10 +57,10 @@ public class UserService {
     // 로그아웃
     public CommonResponseDto<Object> logout(String accessToken) {
 
-        // 토큰 통해 email, get -> 유저 조회
-        String email = jwtUtil.getUserEmail(accessToken);
+        // 토큰 통해 userId get -> 유저 조회
+        Long userId = Long.parseLong(jwtUtil.getUserId(accessToken));
 
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findById(userId)
                 .orElseThrow(()-> new NotFoundException(ErrorCode.USER_NOT_FOUND));
 
         // refreshToken -> null 변경
@@ -66,7 +70,7 @@ public class UserService {
         return commonService.successResponse(SuccessCode.USER_LOGOUT_SUCCESS.getDescription(), HttpStatus.OK, null);
     }
 
-    // accessToken 갱신
+    // 토큰 갱신
     public CommonResponseDto<Object> renewToken(String refreshToken) {
 
         // 토큰 통해 유저 get
@@ -74,11 +78,10 @@ public class UserService {
                 .orElseThrow(() -> new NotFoundException(ErrorCode.TOKEN_NOT_FOUND));
 
         User user = login.getUser();
-        String email = user.getEmail();
-        String role = user.getRole().getKey();
+        Long userid = user.getId();
 
         // accessToken, refreshToken 재발급
-        GeneratedToken token = jwtUtil.generateToken(email, role);
+        GeneratedToken token = jwtUtil.generateToken(userid);
 
         TokenDto tokenDto = TokenDto.builder()
                 .accessToken(token.getAccessToken())
@@ -89,37 +92,45 @@ public class UserService {
         return commonService.successResponse(SuccessCode.USER_RENEW_SUCCESS.getDescription(), HttpStatus.OK, tokenDto);
     }
 
-        public CommonResponseDto<Object> signup(SignupDto signupDto) {
-            if(!findByPhoneNumber(signupDto.getPhoneNumber())){
-                if(!findByNickName(signupDto.getName())){
-                    User user = User.builder()
-                            .phoneNumber(signupDto.getPhoneNumber())
-                            .name(signupDto.getName())
-                            .role(signupDto.getRole())
-                            .email(signupDto.getEmail())
-                            .nickname(signupDto.getNickname())
-                            .build();
-                    userRepository.save(user);
+    // 회원가입
+    public CommonResponseDto<Object> signup(SignupDto signupDto) {
+        if(!findByPhoneNumber(signupDto.getPhoneNumber())){
+            if(!findByNickName(signupDto.getName())){
+                User user = User.builder()
+                        .phoneNumber(signupDto.getPhoneNumber())
+                        .name(signupDto.getName())
+                        .role(signupDto.getRole())
+                        .email(signupDto.getEmail())
+                        .nickname(signupDto.getNickname())
+                        .build();
+                userRepository.save(user);
 
 
-                    return commonService.successResponse(SuccessCode.SIGNUP_SUCCESS.getDescription(),HttpStatus.OK,null);}
-                else{
-                    //이미 가입한 닉네임
-                    return commonService.errorResponse(ErrorCode.EXISTED_NICKNAME.getDescription(), HttpStatus.BAD_REQUEST, null);
-                }
-            }else {
-                //이미 가입한 핸드폰 번호
-                return commonService.errorResponse(ErrorCode.EXISTED_PHONE_NUMBER.getDescription(), HttpStatus.BAD_REQUEST, null);
+                return commonService.successResponse(SuccessCode.SIGNUP_SUCCESS.getDescription(),HttpStatus.OK,null);}
+            else{
+                //이미 가입한 닉네임
+                return commonService.errorResponse(ErrorCode.EXISTED_NICKNAME.getDescription(), HttpStatus.BAD_REQUEST, null);
             }
+        }else {
+            //이미 가입한 핸드폰 번호
+            return commonService.errorResponse(ErrorCode.EXISTED_PHONE_NUMBER.getDescription(), HttpStatus.BAD_REQUEST, null);
         }
+    }
 
-        private boolean findByNickName(String nickname) {
-            User user = userRepository.findByNickname(nickname);
-            return user != null;
-        }
+    private boolean findByNickName(String nickname) {
+        User user = userRepository.findByNickname(nickname);
+        return user != null;
+    }
 
-        private boolean findByPhoneNumber(String phoneNumber) {
-            User user = userRepository.findByPhoneNumber(phoneNumber);
-            return user != null;
-        }
+    private boolean findByPhoneNumber(String phoneNumber) {
+        User user = userRepository.findByPhoneNumber(phoneNumber);
+        return user != null;
+    }
+
+    public CommonResponseDto<Object> addinfo(String email, String picture) {
+        NewUserDto newUserDto = new NewUserDto();
+        newUserDto.setEmail(email);
+        newUserDto.setPicture(picture);
+        return commonService.successResponse(SuccessCode.NEW_USER_SUCCESS.getDescription(), HttpStatus.OK, newUserDto);
+    }
 }
