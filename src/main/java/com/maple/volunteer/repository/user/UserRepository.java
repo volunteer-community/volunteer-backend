85501520e3e018f8d1f456ec946a451073109a9a
dhdhk <hee0zzang@gmail.com>
2023-10-31 23:05:57 +0900

    :sparkles: add duplication check in controller

--- src/main/java/com/maple/volunteer/repository/user/UserRepository.java
+++ src/main/java/com/maple/volunteer/repository/user/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.Optional;
 
@@ -21,4 +22,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findActiveUserByEmail(@Param("email") String email);
 
     Page<User> findAll(Pageable pageable);
+
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false  AND u.nickname = :nickname")
+    Optional<User> findNickname(@Param("nickname") String nickname);
+
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false  AND u.phoneNumber = :phone")
+    Optional<User> findPhone(@Param("phone") String phone);
 }
