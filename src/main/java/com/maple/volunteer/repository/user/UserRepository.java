03916b8e002df82fdd503b9bd8afe3563cadc032
dhdhk <hee0zzang@gmail.com>
2023-11-01 21:49:08 +0900

    :sparkles: add modify userInfo in userController

--- src/main/java/com/maple/volunteer/repository/user/UserRepository.java
+++ src/main/java/com/maple/volunteer/repository/user/UserRepository.java
@@ -1,22 +1,19 @@
 package com.maple.volunteer.repository.user;
 
 import com.maple.volunteer.domain.user.User;
-import com.maple.volunteer.dto.user.UserDto;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String userEmail);
-    User findByPhoneNumber(String phoneNumber);
-    User findByNickname(String nickname);
+
 
     @Query("SELECT u FROM User u WHERE u.isDeleted = false AND u.email = :email")
     Optional<User> findActiveUserByEmail(@Param("email") String email);
@@ -28,4 +25,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.isDeleted = false  AND u.phoneNumber = :phone")
     Optional<User> findPhone(@Param("phone") String phone);
+
+    @Modifying
+    @Query("UPDATE User u set u.phoneNumber = :phone, u.nickname = :nickname where u.id = :id")
+    void updateUserInfo(@Param("phone") String phone, @Param("nickname") String nickname, @Param("id") Long id);
 }
