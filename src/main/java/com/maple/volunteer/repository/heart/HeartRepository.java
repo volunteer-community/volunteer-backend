f9a409a8c2f4fe8301af039f24ac314b4c5a0fe4
dannab <koogoori54@gmail.com>
2023-11-03 15:42:39 +0900

    :recycle: 미사용 import 삭제

--- src/main/java/com/maple/volunteer/repository/heart/HeartRepository.java
+++ src/main/java/com/maple/volunteer/repository/heart/HeartRepository.java
@@ -1,12 +1,10 @@
 package com.maple.volunteer.repository.heart;
 
 import com.maple.volunteer.domain.heart.Heart;
-import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
