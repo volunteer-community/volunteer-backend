6ac6325efcb698e6a1d1cec433ed7b71b5cdd3f5
Llimy1 <alsgur990104@gmail.com>
2023-11-23 21:55:05 +0900

    :rocket: successHandler redirect 변경

--- src/main/java/com/maple/volunteer/security/oauth2/MyAuthenticationFailureHandler.java
+++ src/main/java/com/maple/volunteer/security/oauth2/MyAuthenticationFailureHandler.java
@@ -15,6 +15,6 @@ import java.io.IOException;
 public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.sendRedirect("http://localhost:3000/fail");
+        response.sendRedirect("https://volunteer-frontend.vercel.app/");
     }
 }
