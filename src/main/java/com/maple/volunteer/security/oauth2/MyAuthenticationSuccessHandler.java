d52ca32c43267c029f58c41ad0308d449c31276d
Llimy1 <alsgur990104@gmail.com>
2023-12-06 14:50:49 +0900

    :rocket: local test url 설정

--- src/main/java/com/maple/volunteer/security/oauth2/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/maple/volunteer/security/oauth2/MyAuthenticationSuccessHandler.java
@@ -36,9 +36,9 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
         String name = oAuth2User.getAttribute("name");
         String picture = oAuth2User.getAttribute("picture");
 
-        boolean testEamil = email.equals("1mwdkim1@gmail.com") || email.equals("alsgur990104@gmail.com") || email.equals("koogoori54@gmail.com");
+//        boolean testEamil = email.equals("1mwdkim1@gmail.com") || email.equals("alsgur990104@gmail.com") || email.equals("koogoori54@gmail.com");
 
-        boolean local = testEamil && provider.equals("google");
+//        boolean local = testEamil && provider.equals("google");
 
 
         // 회원이 존재하지 않으면
@@ -56,25 +56,26 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
         } else {
 
-            if (local) {
-
-                TokenDto tokenDto = userService.login(email, provider);
-
-                String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login/loading")
-                        .queryParam("trigger", true)
-                        .queryParam("accessToken", tokenDto.getAccessToken())
-                        .queryParam("accessTokenExpireTime", tokenDto.getAccessTokenExpireTime())
-                        .queryParam("refreshToken", tokenDto.getRefreshToken())
-                        .queryParam("refreshTokenExpireTime", tokenDto.getRefreshTokenExpireTime())
-                        .build()
-                        .encode(StandardCharsets.UTF_8)
-                        .toUriString();
-                getRedirectStrategy().sendRedirect(request, response, targetUrl);
-            }
+//            if (local) {
+//
+//                TokenDto tokenDto = userService.login(email, provider);
+//
+//                String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login/loading")
+//                        .queryParam("trigger", true)
+//                        .queryParam("accessToken", tokenDto.getAccessToken())
+//                        .queryParam("accessTokenExpireTime", tokenDto.getAccessTokenExpireTime())
+//                        .queryParam("refreshToken", tokenDto.getRefreshToken())
+//                        .queryParam("refreshTokenExpireTime", tokenDto.getRefreshTokenExpireTime())
+//                        .build()
+//                        .encode(StandardCharsets.UTF_8)
+//                        .toUriString();
+//                getRedirectStrategy().sendRedirect(request, response, targetUrl);
+//            }
 
             TokenDto tokenDto = userService.login(email, provider);
 
-            String targetUrl = UriComponentsBuilder.fromUriString("https://ecof.site/login/loading")
+            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login/loading")
+//            String targetUrl = UriComponentsBuilder.fromUriString("https://ecof.site/login/loading")
 
                     .queryParam("trigger", true)
                     .queryParam("accessToken", tokenDto.getAccessToken())
